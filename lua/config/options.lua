30f15460114ceb98d789462d8a9b6fe35a8f4a26
jameseec <159089356+jameseec@users.noreply.github.com>
2026-03-03 13:07:47 -0800

    Configure clipboard and editor options
    Added clipboard support and updated various options.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,20 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.clipboard = "unnamedplus"
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  },
+}
+vim.opt.autowrite = true
+vim.opt.undofile = true
+vim.g.ts_install_compilers = { "gcc" }
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4         -- 4 spaces for indent
+vim.opt.expandtab = true       -- use spaces instead of tabs (check if your assignment requires tabs)
